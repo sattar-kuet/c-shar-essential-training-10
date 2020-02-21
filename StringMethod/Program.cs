d588e252659300f91e7958a08a36627aa82e0ecd
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:51:09 +0600

    first commit

---
+++ StringMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "C# Programming"; // 14
+            int l = subject.Length;
+            string modified_subject = subject.ToLower();
+            string modified_subject2 = subject.ToUpper();
+            string modified_subject3 =  subject.Replace("C#","C++");
+            Console.WriteLine(modified_subject3);
+
+
+        }
+    }
+}
